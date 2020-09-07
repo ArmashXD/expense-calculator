614a56cbcd9525726d72403a093ef0a3d3ccb348
unknown <armashash@gmail.com>
2020-09-07 09:54:36 +0500

    Updates

---
+++ precache-manifest.2d92b3b11a5aef95eace6c56f1960290.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fd4d31d6296d78a2c166c200c2f0abe",
+    "url": "/expense-calculator/index.html"
+  },
+  {
+    "revision": "29ca35407df15570f6c4",
+    "url": "/expense-calculator/static/css/main.eff945fc.chunk.css"
+  },
+  {
+    "revision": "64629b05b432c44e1bd6",
+    "url": "/expense-calculator/static/js/2.4599387a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/expense-calculator/static/js/2.4599387a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29ca35407df15570f6c4",
+    "url": "/expense-calculator/static/js/main.c1472536.chunk.js"
+  },
+  {
+    "revision": "bd340826dc100f4faa44",
+    "url": "/expense-calculator/static/js/runtime-main.62647b6f.js"
+  }
+]);
